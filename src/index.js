57cc8ceb49ad0663e9fdc48c241744f41c94cdc3
Elana Kopelevich <ekopelevich@gmail.com>
2018-08-30 08:07:20 -0600

    redux version

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducers from './reducers'
+import App from './App'
+
+// Creating the store using the reducers info.
+// That's because reducers are the building blocks of a Redux Store.
+const store = createStore(reducers)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
